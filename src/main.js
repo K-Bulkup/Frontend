73500600aba6f74517db9c5eef4a2dead99f87d5
YoungE <56716209+kchy0625@users.noreply.github.com>
2025-07-25 17:38:59 +0900

    Fix : pinia-plugin-persistedstate 제거

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+
 
 import App from "./App.vue";
 import router from "./router";
@@ -13,7 +13,7 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 const app = createApp(App);
 const pinia = createPinia();
 
-pinia.use(piniaPluginPersistedstate);
+
 
 app.use(pinia);
 app.use(router);
