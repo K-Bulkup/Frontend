53c348b328810272ec6c2ddada614c5b9b7afea0
chaeniiiii <kce2360@gmail.com>
2025-07-31 00:49:59 +0900

    Feat : 트레이너 마이페이지 구현

--- src/views/trainer/TrainerMyPage.vue
+++ src/views/trainer/TrainerMyPage.vue
@@ -1,4 +1,16 @@
-<script setup></script>
+<script setup>
+import TrainerGreeting from "@/components/trainer/mypage/TrainerGreeting.vue";
+import TrainerInfo from "@/components/trainer/mypage/TrainerInfo.vue";
+import TrainerMenu from "@/components/trainer/mypage/TrainerMenu.vue";
+</script>
+
 <template>
-  <div>트레이너 마이 페이지입니다.</div>
+  <div class="min-h-screen pb-20 text-white">
+    <!-- 트레이너 인사말 -->
+    <TrainerGreeting></TrainerGreeting>
+    <!-- 트레이너 정보 섹션 -->
+    <TrainerInfo></TrainerInfo>
+    <!-- 트레이너 사용 메뉴 -->
+    <TrainerMenu></TrainerMenu>
+  </div>
 </template>
