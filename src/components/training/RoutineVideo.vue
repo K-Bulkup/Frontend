ca37bb7a8fd86af9a5ae6f22b968c2767df22d1f
sangeune <sangeun504@naver.com>
2025-07-30 15:24:15 +0900

    Feat : 루틴 수행 상세 페이지 구현

---
+++ src/components/training/RoutineVideo.vue
@@ -0,0 +1,39 @@
+<script setup>
+defineProps({
+  videoUrl: String,
+  thumbnail: String,
+});
+
+const emit = defineEmits(["play"]);
+
+const handlePlayClick = () => {
+  emit("play");
+};
+</script>
+
+<template>
+  <div class="my-6">
+    <h3 class="mb-4 text-heading text-white">참고 영상</h3>
+    <div
+      @click="handlePlayClick"
+      class="relative aspect-video cursor-pointer overflow-hidden rounded-xl bg-gray-200"
+    >
+      <!-- 썸네일이 있는 경우 -->
+      <img
+        v-if="thumbnail"
+        :src="thumbnail"
+        alt="비디오 썸네일"
+        class="h-full w-full object-cover"
+      />
+
+      <!-- 썸네일이 없는 경우 fallback UI -->
+      <div v-else class="flex h-full w-full items-center justify-center">
+        <img
+          src="@/assets/images/youtube_white.svg"
+          alt="유튜브 아이콘"
+          class="h-12 w-12"
+        />
+      </div>
+    </div>
+  </div>
+</template>
