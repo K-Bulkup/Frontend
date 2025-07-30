81a14bc58a6e8da09103b5ac427f76b31b3bbf70
Jung-kr <jungwy98@gmail.com>
2025-07-30 14:55:52 +0900

    Feat: Pinia 상태 관리 라이브러리 도입 및 전역 등록

--- src/main.js
+++ src/main.js
@@ -10,8 +10,9 @@ import "@/plugins/fontawesome";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 const app = createApp(App);
+const pinia = createPinia();
 
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 
 app.component("font-awesome-icon", FontAwesomeIcon);
