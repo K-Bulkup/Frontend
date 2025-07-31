f9fba260c0a8477df0fb8a3a2c0ccea46a6682ba
chaeniiiii <kce2360@gmail.com>
2025-07-31 15:08:05 +0900

    Feat : BaseInput 입력창 크기 수정

--- src/components/common/BaseInput.vue
+++ src/components/common/BaseInput.vue
@@ -23,32 +23,32 @@ const inputAttrs = useAttrs();
 </script>
 
 <template>
-  <div class="w-full">
+  <div class="flex w-full flex-col items-center pb-3">
     <div
-      class="flex flex-col gap-1 rounded-xl border bg-gray-100 px-4 py-3"
+      class="flex items-center gap-3 rounded-xl border bg-gray-100 px-4 py-3"
       :class="[
         isInvalid ? 'border-error' : 'border-gray-200',
         'transition duration-200',
       ]"
+      style="width: 360px; height: 60px"
     >
-      <div class="flex items-center">
-        <slot name="icon" />
+      <slot name="icon" />
 
-        <input
-          v-bind="inputAttrs"
-          :value="modelValue"
-          @input="(e) => emit('update:modelValue', e.target.value)"
-          :type="type"
-          :placeholder="placeholder"
-          class="placeholder-gray700 ml-2 flex-1 bg-transparent text-subtext text-black outline-none"
-        />
-      </div>
+      <input
+        v-bind="inputAttrs"
+        :value="modelValue"
+        @input="(e) => emit('update:modelValue', e.target.value)"
+        :type="type"
+        :placeholder="placeholder"
+        class="placeholder-gray700 flex-1 bg-transparent text-sm text-black outline-none"
+        :class="$slots.icon ? 'ml-2' : ''"
+      />
     </div>
-    <p
-      v-if="isInvalid && errorMessage"
-      class="flex items-center gap-1 pl-[16px] pt-[6px] text-caption text-error"
-    >
-      {{ errorMessage }}
-    </p>
+    <!-- 에러 메시지를 왼쪽 정렬로 변경 -->
+    <div v-if="isInvalid && errorMessage" class="w-full max-w-[360px]">
+      <p class="pl-[5px] pt-[6px] text-left text-caption text-error">
+        {{ errorMessage }}
+      </p>
+    </div>
   </div>
 </template>
