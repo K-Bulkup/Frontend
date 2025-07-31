60f8686f07ee09db6b1abd556d87137d98f1b81e
chaeniiiii <kce2360@gmail.com>
2025-07-31 15:09:39 +0900

    Feat : 트레이너 자격증 인증 페이지 구현

--- src/views/trainer/TrainerVerifyPage.vue
+++ src/views/trainer/TrainerVerifyPage.vue
@@ -1,4 +1,24 @@
-<script setup></script>
+<script setup>
+import BaseHeader from "@/components/common/BaseHeader.vue";
+import BaseStatusMessage from "@/components/common/BaseStatusMessage.vue";
+import CertificatesInfo from "@/components/trainer/certificates/CertificatesInfo.vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const handleBack = () => {
+  router.go(-1); // 이전 페이지로 이동
+};
+</script>
+
 <template>
-  <div>트레이너 인증 페이지입니다.</div>
+  <BaseHeader title="자격증 검증" @back="handleBack" />
+
+  <!-- 페이지 설명 -->
+  <BaseStatusMessage
+    title="자격 정보를 입력해주세요"
+    subtitle="합격증 번호와 발급 번호를 입력해주세요."
+  />
+
+  <CertificatesInfo />
 </template>
