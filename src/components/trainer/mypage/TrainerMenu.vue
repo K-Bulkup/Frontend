91387e6015eec1251062e4c3531e979b042cb476
chaeniiiii <kce2360@gmail.com>
2025-08-01 10:50:17 +0900

    Feat : 트레이너 마이페이지 메뉴 아이콘 추가

--- src/components/trainer/mypage/TrainerMenu.vue
+++ src/components/trainer/mypage/TrainerMenu.vue
@@ -1,5 +1,10 @@
 <script setup>
 import { useRouter } from "vue-router";
+
+import certificationIcon from "@/assets/images/trainer/mypage/certificationIcon.svg";
+import memberIcon from "@/assets/images/trainer/mypage/memberIcon.svg";
+import qnaIcon from "@/assets/images/trainer/mypage/qnaIcon.svg";
+
 const router = useRouter();
 
 // 인증 페이지로 이동
@@ -15,6 +20,7 @@ const goToVerify = () => {
       <div
         class="mb-3 flex h-16 w-16 items-center justify-center rounded-full bg-gray-700 text-white"
       >
+        <img :src="memberIcon" alt="인증" class="h-6 w-6" />
         <i class="fas fa-users text-xl"></i>
       </div>
       <span class="text-sm text-white">수강생관리</span>
@@ -25,6 +31,7 @@ const goToVerify = () => {
       <div
         class="mb-3 flex h-16 w-16 items-center justify-center rounded-full bg-gray-100 text-black"
       >
+        <img :src="certificationIcon" alt="인증" class="h-8 w-8" />
         <i class="fas fa-check text-xl"></i>
       </div>
       <span class="text-sm text-white">인증</span>
@@ -35,6 +42,7 @@ const goToVerify = () => {
       <div
         class="mb-3 flex h-16 w-16 items-center justify-center rounded-full bg-gray-700 text-white"
       >
+        <img :src="qnaIcon" alt="인증" class="h-6 w-6" />
         <i class="fas fa-comments text-xl"></i>
       </div>
       <span class="text-sm text-white">QNA</span>
