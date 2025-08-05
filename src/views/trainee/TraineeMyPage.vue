67d26890ef51e7c414bad27dae582d32ee6d5771
Jung-kr <jungwy98@gmail.com>
2025-08-05 19:44:18 +0900

    Feat: 트레이니 마이페이지 개발
    - ProfileCard에 마스코트 캐릭터 이미지 추가
    - CourseList 컴포넌트 반응형 레이아웃 구현 (화면당 3개 코스 표시)
    - 트레이닝 카드 높이 최적화 및 텍스트 크기 조정
    - 진행률 표시 우측 정렬로 변경

--- src/views/trainee/TraineeMyPage.vue
+++ src/views/trainee/TraineeMyPage.vue
@@ -1,4 +1,42 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import ProfileCard from "@/components/trainee/mypage/ProfileCard.vue";
+import CourseList from "@/components/trainee/mypage/CourseList.vue";
+import BaseHeaderWithoutBack from "@/components/common/BaseHeaderWithoutBack.vue";
+
+const courses = ref([
+  {
+    id: 1,
+    title: "강좌이름1강좌이름1",
+    progress: 75,
+  },
+  {
+    id: 2,
+    title: "강좌이름2강좌이름2",
+    progress: 75,
+  },
+  {
+    id: 3,
+    title: "강좌이름3강좌이름3",
+    progress: 75,
+  },
+  {
+    id: 4,
+    title: "강좌이름4강좌이름4",
+    progress: 50,
+  },
+  {
+    id: 5,
+    title: "강좌이름5강좌이름5",
+    progress: 50,
+  },
+]);
+</script>
+
 <template>
-  <div>트레이니 마이 페이지입니다.</div>
+  <div class="p-6">
+    <BaseHeaderWithoutBack title="마이 페이지" />
+    <ProfileCard :userName="'홍길동님은'" :stats="'3대 200Kg'" />
+    <CourseList :courses="courses" class="mt-6" />
+  </div>
 </template>
