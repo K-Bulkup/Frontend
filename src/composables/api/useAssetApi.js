8f4d336349c30ec5db9f55414111b6a73ce1bab5
HyoSangByun <billy04100410@gmail.com>
2025-08-05 10:04:21 +0900

    Refactor : API 호출 변경

--- src/composables/api/useAssetApi.js
+++ src/composables/api/useAssetApi.js
@@ -1,14 +1,11 @@
 import apiClient from "@/plugins/axios";
 
 export const postConnectAccount = async (bankName) => {
-  return await apiClient.post(
-    `http://localhost:8080/api/trainee/assets/account`,
-    {
-      bank: bankName,
-    },
-  );
+  return await apiClient.post(`/api/trainee/assets/account`, {
+    bank: bankName,
+  });
 };
 
 export const getTraineeAsset = async () => {
-  return await apiClient.get(`http://localhost:8080/api/trainee/assets`);
+  return await apiClient.get(`/api/trainee/assets`);
 };
