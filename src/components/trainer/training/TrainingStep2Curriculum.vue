a5e95440254a5fba76bad9f4340e2fcc4b57d099
sangeune <sangeun504@naver.com>
2025-08-07 01:33:12 +0900

    Refactor : 새 파일과 충돌 방지를 위해 기존 파일 이름 변경

--- src/components/trainer/training/TrainingStep2Curriculum.vue
+++ src/components/trainer/training/TrainingStep2Curriculum.vue
@@ -2,7 +2,7 @@
 import { ref } from "vue";
 import BaseStatusMessage from "@/components/common/BaseStatusMessage.vue";
 import BaseFormField from "@/components/common/BaseFormField.vue";
-import TrainerRoutineSection from "./TrainerRoutineSection.vue";
+import TrainerRoutineAddSection from "./TrainerRoutineAddSection.vue";
 
 const trainerName = defineModel("trainerName");
 const description = defineModel("description");
@@ -81,7 +81,7 @@ const handleSectionToggle = (sectionKey) => {
 
       <div>
         <div class="mb-4 text-subtext text-gray-50">루틴</div>
-        <TrainerRoutineSection
+        <TrainerRoutineAddSection
           v-for="section in ROUTINE_SECTIONS"
           :key="section.key"
           :title="section.title"
