d9758a2dbf6c85b4926778ddce8947e0e8e67c56
HyoSangByun <billy04100410@gmail.com>
2025-08-08 11:07:46 +0900

    Feat : 트레이닝 상세페이지에서 QnA 페이지 이동 버튼 추가

--- src/views/trainee/TraineeTrainingDetailPage.vue
+++ src/views/trainee/TraineeTrainingDetailPage.vue
@@ -204,6 +204,10 @@ const startChat = async () => {
     alert("채팅방 생성 중 오류가 발생했습니다. 다시 시도해주세요.");
   }
 };
+
+const goToQnaPage = () => {
+  router.push(`/trainee/mypage/training/${route.params.trainingId}/qna`);
+};
 </script>
 
 <template>
@@ -262,7 +266,13 @@ const startChat = async () => {
           <span>|</span>
           <span>{{ trainingData.studentCount }}명 수강</span>
           <span>|</span>
-          <span>{{ trainingData.totalWeeks }}주</span>
+          <!-- Q&A 버튼 -->
+          <button
+            @click="goToQnaPage"
+            class="rounded-full bg-primary px-3 py-1.5 text-xs font-semibold text-black shadow"
+          >
+            Q&A
+          </button>
         </div>
       </div>
 
