d8256f4cfb94f80983ea302ac3a3785829a932d0
sangeune <sangeun504@naver.com>
2025-08-11 23:29:10 +0900

    Refactor : 트레이닝 등록 페이지 문구 및 색상 수정

--- src/views/trainer/TrainerTrainingInputPage.vue
+++ src/views/trainer/TrainerTrainingInputPage.vue
@@ -177,7 +177,7 @@ const handleCompletion = () => {
 </script>
 
 <template>
-  <div class="flex min-h-screen flex-col bg-realBlack px-6 pb-20 pt-4">
+  <div class="flex min-h-screen flex-col px-6 pb-20 pt-4">
     <BaseHeader v-if="step < 4" title="트레이닝 오픈" @back="handleGoBack" />
 
     <div class="flex-grow">
