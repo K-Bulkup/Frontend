4d6dc26bcbe1856ee762651eb9d626cae9b86c00
YoungE <56716209+kchy0625@users.noreply.github.com>
2025-08-12 13:09:43 +0900

    Refactor : 관리자페이지 디자인 변경

--- src/views/admin/AdminUserStatisticsPage.vue
+++ src/views/admin/AdminUserStatisticsPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen bg-realblack p-6 text-white">
+  <div class="bg-realblack min-h-screen p-6 text-white">
     <h1 class="mb-6 text-3xl font-bold">회원 통계</h1>
 
     <div class="mx-auto max-w-6xl space-y-6">
@@ -16,8 +16,8 @@
           class="rounded-md px-4 py-2 text-sm font-bold transition-colors"
           :class="
             selectedRole === role.key
-              ? 'bg-yellow-500 text-black'
-              : 'bg-gray-200 text-black hover:bg-gray-300'
+              ? 'bg-green-500 text-white'
+              : 'bg-gray-500 text-white'
           "
         >
           {{ role.label }}
@@ -35,11 +35,11 @@
           ]"
           :key="period.key"
           @click="selectPeriod(period.key)"
-          class="rounded-md px-4 py-2 text-sm font-bold transition-colors"
+          class="rounded-md px-2 py-2 text-sm font-bold transition-colors"
           :class="
             selectedPeriod === period.key
-              ? 'bg-yellow-500 text-black'
-              : 'bg-gray-200 text-black hover:bg-gray-300'
+              ? 'bg-green-500 text-white'
+              : 'bg-gray-500 text-white'
           "
         >
           {{ period.label }}
@@ -47,9 +47,9 @@
       </div>
 
       <!-- 요약 통계 -->
-      <div class="rounded-xl bg-realblack p-4 shadow">
+      <div class="rounded-xl bg-gray-800 p-4 shadow">
         <h2 class="mb-2 text-xl font-semibold">요약 통계</h2>
-        <div class="text-gray-600">
+        <div>
           <p>
             오늘 증가한 회원 수:
             <span class="font-bold text-white">{{ todayNewUsers }}</span
@@ -64,7 +64,7 @@
       </div>
 
       <!-- 가입 통계 차트 -->
-      <div class="rounded-xl bg-realblack p-4 shadow">
+      <div class="rounded-xl bg-gray-800 p-4 shadow">
         <h2 class="mb-2 text-xl font-semibold">
           기간별 가입 통계 ({{ periodLabel }})
         </h2>
@@ -80,7 +80,7 @@
 import { ref, onMounted, watch, computed } from "vue";
 import { useAdminUserApi } from "@/composables/api/useAdminUserApi";
 import { Line } from "vue-chartjs";
-import BaseButton from "@/components/common/BaseButton.vue";
+import BaseButton from "@/components/admin/AdminBaseButton.vue";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -129,8 +129,8 @@ const chartData = computed(() => {
     datasets: [
       {
         label: "가입자 수",
-        backgroundColor: "#f87979",
-        borderColor: "#f87979",
+        backgroundColor: "#22e481",
+        borderColor: "#22e481",
         data,
         tension: 0.4,
       },
