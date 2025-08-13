42d56c9cfd9593ef7b66e726a3c4112e696b5899
chaeniiiii <kce2360@gmail.com>
2025-08-13 23:41:49 +0900

    refactor : 탭 컨텐츠 높이 재설정

--- src/components/common/BaseTabNavigation.vue
+++ src/components/common/BaseTabNavigation.vue
@@ -61,7 +61,7 @@ watch(
       </button>
     </div>
     <!-- 탭 컨텐츠 -->
-    <div class="mt-4 max-h-96 overflow-y-auto scrollbar-hide">
+    <div class="my-4 max-h-[480px] overflow-y-auto scrollbar-hide">
       <template v-for="tab in tabs" :key="`content-${tab.id}`">
         <div v-if="activeTab === tab.id">
           <slot :name="tab.id" :tab="tab" :active-tab="activeTab">
