a441e8dac85e5cd86f96b2742fdf918940a15586
HyoSangByun <billy04100410@gmail.com>
2025-08-15 00:41:27 +0900

    Refactor : 리뷰 헤더 통일화

--- src/views/trainee/TraineeReviewPage.vue
+++ src/views/trainee/TraineeReviewPage.vue
@@ -58,10 +58,10 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="flex min-h-screen flex-col px-7 pb-20">
+  <div class="flex min-h-screen flex-col pb-20">
     <BaseHeader title="리뷰 작성" @back="handleBack" />
 
-    <div class="flex-grow">
+    <div class="flex-grow px-7">
       <div class="mb-6">
         <h2 class="mb-6 mt-8 text-subTitle font-semibold">
           {{ reviewData?.title }}
