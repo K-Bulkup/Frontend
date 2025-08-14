334ce5bfe0c90c5bc8d399acb760b7a036fc3f89
HyoSangByun <billy04100410@gmail.com>
2025-08-15 01:12:28 +0900

    Refactor : 헤더 너비 통일화

--- src/views/trainee/TraineeTrainingQuestionInput.vue
+++ src/views/trainee/TraineeTrainingQuestionInput.vue
@@ -56,10 +56,10 @@ const handleSubmit = async () => {
 </script>
 
 <template>
-  <div class="flex min-h-screen flex-col px-7 pb-20">
+  <div class="flex min-h-screen flex-col pb-20">
     <BaseHeader title="잘문 등록하기" @back="handleBack" />
 
-    <div class="flex-grow">
+    <div class="flex-grow px-7">
       <div class="mb-6">
         <!-- Course Title -->
         <h2 class="mb-6 mt-8 text-subTitle font-semibold">
