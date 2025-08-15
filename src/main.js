79332f0cd38e49bccb8cb79a94dc7a732d94d51f
chaeniiiii <kce2360@gmail.com>
2025-08-16 03:04:09 +0900

    fix : axios 설정 변경

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import axios from "axios";
-s;
+
 import App from "./App.vue";
 import router from "./router";
 import { useAuthStore } from "@/stores/auth";
