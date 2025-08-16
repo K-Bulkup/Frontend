6ec18fcd8df30abe6d35576262bf3752012c72c6
chaeniiiii <kce2360@gmail.com>
2025-08-16 09:26:13 +0900

    fix : 환경 변수 설정

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import axios from "axios";
 
 import App from "./App.vue";
 import router from "./router";
@@ -11,17 +10,10 @@ import "@/plugins/fontawesome";
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-// 환경별 axios 기본 URL 설정
-if (import.meta.env.PROD) {
-  axios.defaults.baseURL = "http://43.201.172.152:8080";
-}
-
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
-
-// 앱 시작 시 localStorage에서 토큰을 확인하고 사용자 정보를 가져옵니다.
 app.use(router);
 
 app.component("font-awesome-icon", FontAwesomeIcon);
